261d087d8c4325010c3a8f74c74b0c909b7a85dd
唐成 <tangcheng2005@gmail.com>
2014-07-25 00:10:42 +0800

    调整IpcamMediaOsd类的私有成员，准备添加属性。

--- src/hi3518/media_osd.c
+++ src/hi3518/media_osd.c
@@ -20,6 +20,11 @@ enum
 typedef struct _IpcamMediaOsdPrivate
 {
     IpcamOsdFont *osd_font;
+    guint32 font_size;
+    RGN_HANDLE RgnHandle;
+    VENC_GRP VencGrp;
+    RGN_ATTR_S stRgnAttr;
+    RGN_CHN_ATTR_S stChnAttr;
 } IpcamMediaOsdPrivate;
 
 static void ipcam_iosd_interface_init(IpcamIOSDInterface *iface);
@@ -32,6 +37,9 @@ G_DEFINE_TYPE_WITH_CODE(IpcamMediaOsd, ipcam_media_osd, G_TYPE_OBJECT,
 
 static void ipcam_media_osd_init(IpcamMediaOsd *self)
 {
+    IpcamMediaOsdPrivate *priv = IPCAM_MEDIA_OSD_GET_PRIVATE(self);
+    priv->RgnHandle = 0;
+    priv->VencGrp = 0;
 }
 /*
 static void ipcam_media_osd_get_property(GObject    *object,
@@ -97,58 +105,51 @@ static void ipcam_media_osd_class_init(IpcamMediaOsdClass *klass)
 static gint32 ipcam_media_osd_start(IpcamMediaOsd *self)
 {
     HI_S32 s32Ret = HI_FAILURE;
-    RGN_HANDLE RgnHandle;
-    VENC_GRP VencGrp;
-    RGN_ATTR_S stRgnAttr;
     MPP_CHN_S stChn;
-    RGN_CHN_ATTR_S stChnAttr;
     IpcamMediaOsdPrivate *priv = IPCAM_MEDIA_OSD_GET_PRIVATE(self);
 
     priv->osd_font = ipcam_osd_font_new();
     g_object_ref(priv->osd_font);
 
-    stRgnAttr.enType = OVERLAY_RGN;
-    stRgnAttr.unAttr.stOverlay.enPixelFmt = PIXEL_FORMAT_RGB_1555;
-    stRgnAttr.unAttr.stOverlay.stSize.u32Width  = 512;
-    stRgnAttr.unAttr.stOverlay.stSize.u32Height = 32;
-    stRgnAttr.unAttr.stOverlay.u32BgColor = 0x7FFF;
+    priv->stRgnAttr.enType = OVERLAY_RGN;
+    priv->stRgnAttr.unAttr.stOverlay.enPixelFmt = PIXEL_FORMAT_RGB_1555;
+    priv->stRgnAttr.unAttr.stOverlay.stSize.u32Width  = 512;
+    priv->stRgnAttr.unAttr.stOverlay.stSize.u32Height = 80;
+    priv->stRgnAttr.unAttr.stOverlay.u32BgColor = 0x7FFF;
 
-    RgnHandle = 0;
-
-    s32Ret = HI_MPI_RGN_Create(RgnHandle, &stRgnAttr);
+    s32Ret = HI_MPI_RGN_Create(priv->RgnHandle, &priv->stRgnAttr);
     if(HI_SUCCESS != s32Ret)
     {
-        g_critical("HI_MPI_RGN_Create (%d) failed with %#x!\n", RgnHandle, s32Ret);
+        g_critical("HI_MPI_RGN_Create (%d) failed with %#x!\n", priv->RgnHandle, s32Ret);
         return s32Ret;
     }
 
-    VencGrp = 0;
     stChn.enModId = HI_ID_GROUP;
-    stChn.s32DevId = VencGrp;
+    stChn.s32DevId = priv->VencGrp;
     stChn.s32ChnId = 0;
 
-    memset(&stChnAttr,0,sizeof(stChnAttr));
-    stChnAttr.bShow = HI_TRUE;
-    stChnAttr.enType = OVERLAY_RGN;
-    stChnAttr.unChnAttr.stOverlayChn.stPoint.s32X = START_POINT_X_OFFSET;
-    stChnAttr.unChnAttr.stOverlayChn.stPoint.s32Y = START_POINT_Y_OFFSET;
-    stChnAttr.unChnAttr.stOverlayChn.u32BgAlpha = 0;
-    stChnAttr.unChnAttr.stOverlayChn.u32FgAlpha = 128;
-    stChnAttr.unChnAttr.stOverlayChn.u32Layer = 0;
+    memset(&priv->stChnAttr, 0, sizeof(priv->stChnAttr));
+    priv->stChnAttr.bShow = HI_TRUE;
+    priv->stChnAttr.enType = OVERLAY_RGN;
+    priv->stChnAttr.unChnAttr.stOverlayChn.stPoint.s32X = START_POINT_X_OFFSET;
+    priv->stChnAttr.unChnAttr.stOverlayChn.stPoint.s32Y = START_POINT_Y_OFFSET;
+    priv->stChnAttr.unChnAttr.stOverlayChn.u32BgAlpha = 0;
+    priv->stChnAttr.unChnAttr.stOverlayChn.u32FgAlpha = 128;
+    priv->stChnAttr.unChnAttr.stOverlayChn.u32Layer = 0;
 
-    stChnAttr.unChnAttr.stOverlayChn.stQpInfo.bAbsQp = HI_FALSE;
-    stChnAttr.unChnAttr.stOverlayChn.stQpInfo.s32Qp  = 0;
+    priv->stChnAttr.unChnAttr.stOverlayChn.stQpInfo.bAbsQp = HI_FALSE;
+    priv->stChnAttr.unChnAttr.stOverlayChn.stQpInfo.s32Qp  = 0;
 
-    stChnAttr.unChnAttr.stOverlayChn.stInvertColor.stInvColArea.u32Width = 16;
-    stChnAttr.unChnAttr.stOverlayChn.stInvertColor.stInvColArea.u32Height = 16;
-    stChnAttr.unChnAttr.stOverlayChn.stInvertColor.u32LumThresh = 48;
-    stChnAttr.unChnAttr.stOverlayChn.stInvertColor.enChgMod = LESSTHAN_LUM_THRESH;
-    stChnAttr.unChnAttr.stOverlayChn.stInvertColor.bInvColEn = HI_TRUE;
+    priv->stChnAttr.unChnAttr.stOverlayChn.stInvertColor.stInvColArea.u32Width = 16;
+    priv->stChnAttr.unChnAttr.stOverlayChn.stInvertColor.stInvColArea.u32Height = 16;
+    priv->stChnAttr.unChnAttr.stOverlayChn.stInvertColor.u32LumThresh = 48;
+    priv->stChnAttr.unChnAttr.stOverlayChn.stInvertColor.enChgMod = LESSTHAN_LUM_THRESH;
+    priv->stChnAttr.unChnAttr.stOverlayChn.stInvertColor.bInvColEn = HI_TRUE;
 
-    s32Ret = HI_MPI_RGN_AttachToChn(RgnHandle, &stChn, &stChnAttr);
+    s32Ret = HI_MPI_RGN_AttachToChn(priv->RgnHandle, &stChn, &priv->stChnAttr);
     if(HI_SUCCESS != s32Ret)
     {
-        g_critical("HI_MPI_RGN_AttachToChn (%d) failed with %#x!\n", RgnHandle, s32Ret);
+        g_critical("HI_MPI_RGN_AttachToChn (%d) failed with %#x!\n", priv->RgnHandle, s32Ret);
         return HI_FAILURE;
     }
     return HI_SUCCESS;
@@ -157,12 +158,10 @@ static gint32 ipcam_media_osd_set_content(IpcamMediaOsd *self, const gchar *cont
 {
     HI_S32 s32Ret = HI_FAILURE;
     gboolean bRet = FALSE;
-    RGN_HANDLE RgnHandle;
     BITMAP_S stBitmap;
     IpcamMediaOsdPrivate *priv = IPCAM_MEDIA_OSD_GET_PRIVATE(self);
     g_return_val_if_fail((NULL != priv->osd_font), s32Ret);
 
-    RgnHandle = 0;
     stBitmap.enPixelFormat = PIXEL_FORMAT_RGB_1555;
     bRet = ipcam_osd_font_render_text(priv->osd_font,
                                       content,
@@ -171,7 +170,7 @@ static gint32 ipcam_media_osd_set_content(IpcamMediaOsd *self, const gchar *cont
                                       &stBitmap.u32Height);
     if (bRet)
     {
-        s32Ret = HI_MPI_RGN_SetBitMap(RgnHandle, &stBitmap);
+        s32Ret = HI_MPI_RGN_SetBitMap(priv->RgnHandle, &stBitmap);
         if(s32Ret != HI_SUCCESS)
         {
             g_critical("HI_MPI_RGN_SetBitMap failed with %#x!\n", s32Ret);
@@ -184,27 +183,23 @@ static gint32 ipcam_media_osd_set_content(IpcamMediaOsd *self, const gchar *cont
 static gint32 ipcam_media_osd_stop(IpcamMediaOsd *self)
 {
     HI_S32 s32Ret = HI_FAILURE;
-    RGN_HANDLE RgnHandle;
-    VENC_GRP VencGrp;
     MPP_CHN_S stChn;
     IpcamMediaOsdPrivate *priv = IPCAM_MEDIA_OSD_GET_PRIVATE(self);
 
-    RgnHandle = 0;
-    VencGrp = 0;
     stChn.enModId = HI_ID_GROUP;
-    stChn.s32DevId = VencGrp;
+    stChn.s32DevId = priv->VencGrp;
     stChn.s32ChnId = 0;
 
-    s32Ret = HI_MPI_RGN_DetachFrmChn(RgnHandle, &stChn);
+    s32Ret = HI_MPI_RGN_DetachFrmChn(priv->RgnHandle, &stChn);
     if(HI_SUCCESS != s32Ret)
     {
-        g_critical("HI_MPI_RGN_DetachFrmChn (%d) failed with %#x!\n", RgnHandle, s32Ret);
+        g_critical("HI_MPI_RGN_DetachFrmChn (%d) failed with %#x!\n", priv->RgnHandle, s32Ret);
         return s32Ret;
     }
-    s32Ret = HI_MPI_RGN_Destroy(RgnHandle);
+    s32Ret = HI_MPI_RGN_Destroy(priv->RgnHandle);
     if (HI_SUCCESS != s32Ret)
     {
-        g_critical("HI_MPI_RGN_Destroy [%d] failed with %#x\n", RgnHandle, s32Ret);
+        g_critical("HI_MPI_RGN_Destroy [%d] failed with %#x\n", priv->RgnHandle, s32Ret);
     }
 
     g_object_unref(priv->osd_font);
