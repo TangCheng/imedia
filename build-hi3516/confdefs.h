ef35db7c93e943bf59b9b415fd881f4df47ceb2d
TangCheng <tangcheng@localhost.localdomain>
2014-09-12 15:13:53 +0800

    调整项目配置文件，使能hi3518或者hi3516时，使用同一个目录下的文件，但定义不同的宏。

---
+++ build-hi3516/confdefs.h
@@ -0,0 +1,21 @@
+/* confdefs.h */
+#define PACKAGE_NAME "imedia"
+#define PACKAGE_TARNAME "imedia"
+#define PACKAGE_VERSION "0.1.00"
+#define PACKAGE_STRING "imedia 0.1.00"
+#define PACKAGE_BUGREPORT ""
+#define PACKAGE_URL ""
+#define PACKAGE "imedia"
+#define VERSION "0.1.00"
+#define STDC_HEADERS 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRING_H 1
+#define HAVE_MEMORY_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_STDINT_H 1
+#define HAVE_UNISTD_H 1
+#define HAVE_DLFCN_H 1
+#define LT_OBJDIR ".libs/"
